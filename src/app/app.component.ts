cee2a9fd089b2b6689275f0f3867c4023f4c63ef
Div <m.divagar@gmail.com>
2017-11-01 20:01:48 +0530

    integrate reveal.js.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Reveal from 'reveal.js';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  ngOnInit() {
+    // Full list of configuration options available at:
+    // https://github.com/hakimel/reveal.js#configuration
+    Reveal.initialize({
+      controls: true,
+      progress: true,
+      history: true,
+      center: true,
+      transition: 'slide', // none/fade/slide/convex/concave/zoom
+      //Optional reveal.js plugins
+      // dependencies: [
+      //   { src: '../../node_modules/reveal.js/lib/js/classList.js', condition: function () { return !document.body.classList; } },
+      //   { src: '../../node_modules/reveal.js/plugin/markdown/marked.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+      //   { src: '../../node_modules/reveal.js/plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+      //   {
+      //     src: '../../node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function () {
+      //       [].forEach.call(document.querySelectorAll('.highlight, pre code'), function (v, i) {
+      //         hljs.highlightBlock(v);
+      //       });
+      //     }
+      //   },
+      //   { src: 'plugin/zoom-js/zoom.js', async: true },
+      //   { src: 'plugin/notes/notes.js', async: true }
+      // ]
+    });
+  }
+
 }
