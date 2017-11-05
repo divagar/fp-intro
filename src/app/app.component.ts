757de2e7e81a4d4318a6417b9e1193d93e73cd65
Div <m.divagar@gmail.com>
2017-11-05 23:02:34 +0530

    integrating monaco editor.
    Signed-off-by: Div <m.divagar@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,25 @@ import * as Reveal from 'reveal.js';
 })
 export class AppComponent {
 
+  editorOptions = {
+    theme: 'vs-dark',
+    language: 'javascript',
+    autoIndent: true,
+    fontSize: '20px',
+    fontWeight: 400,
+    lineNumbers: 'off',
+    minimap: {enabled: false},
+    scrollbar: {vertical: 'hidden', verticalScrollbarSize: '1px'}
+  }
+  code: string = 'function greetings(message) { return \'Hello \' + message; } \
+  \n\nvar morningGreeting = greetings(\'Good morning\');\
+  \nvar noonGreeting = greetings(\'Good afternoon\');\
+  \nvar eveningGreeting = greetings(\'Good evening\');\
+  \n\
+  \nconsole.log(morningGreeting);\
+  \nconsole.log(noonGreeting);\
+  \nconsole.log(eveningGreeting);';
+
   ngOnInit() {
     // Full list of configuration options available at:
     // https://github.com/hakimel/reveal.js#configuration
@@ -19,18 +38,13 @@ export class AppComponent {
       transition: 'slide', // none/fade/slide/convex/concave/zoom
       //Optional reveal.js plugins
       // dependencies: [
-      //   { src: '../../node_modules/reveal.js/lib/js/classList.js', condition: function () { return !document.body.classList; } },
-      //   { src: '../../node_modules/reveal.js/plugin/markdown/marked.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
-      //   { src: '../../node_modules/reveal.js/plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
       //   {
       //     src: '../../node_modules/reveal.js/plugin/highlight/highlight.js', async: true, callback: function () {
       //       [].forEach.call(document.querySelectorAll('.highlight, pre code'), function (v, i) {
-      //         hljs.highlightBlock(v);
+      //         //hljs.highlightBlock(v);
       //       });
       //     }
-      //   },
-      //   { src: 'plugin/zoom-js/zoom.js', async: true },
-      //   { src: 'plugin/notes/notes.js', async: true }
+      //   }
       // ]
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MonacoEditorModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
