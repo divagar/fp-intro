93f0382fc78f75db697dba19733941135c8b51c9
Div <m.divagar@gmail.com>
2017-11-26 19:20:39 +0530

    FP: function composition

--- src/app/slides/compose.component.ts
+++ src/app/slides/compose.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
 import { HttpService } from '../app.http.service';
 import * as Reveal from 'reveal.js';
 import * as appConfig from '../../../config.json';
@@ -10,5 +11,46 @@ import * as appConfig from '../../../config.json';
 })
 
 export class ComposeComponent {
+    gistUrl: string;
+    mainCode: string;
+    consoleOutput: string;
+    jsEditorOptions = {};
+    htmlEditorOptions = {};
 
+    constructor(private httpService: HttpService,
+                private appCom: AppComponent) {
+        this.gistUrl = (<any>appConfig).gist.apiUrl;
+        this.initEditor();
+    }
+
+    ngOnInit() {
+        var that = this;
+        Reveal.addEventListener('slide_com', function () {
+            that.initEditor();
+            that.getCode('4c3b68f11e404db9431bafca17644e7d').then(
+                (val) => that.mainCode = String(val)
+            );
+        }, false);
+    }
+
+    initEditor() {
+        this.jsEditorOptions = Object.assign({}, (<any>appConfig).jsEditorOptions);
+        this.htmlEditorOptions = Object.assign({}, (<any>appConfig).htmlEditorOptions);
+    }
+
+    getCode(gistId) {
+        var url = this.gistUrl + gistId + '/raw';
+        return new Promise((resolve, reject) => {
+            this.httpService.get(url).subscribe(
+                (val) => { resolve(val); },
+                (err) => { reject(err) }
+            );
+        })
+    }
+
+    run() {
+        this.appCom.fpEval(this.mainCode).then(
+            (val) => this.consoleOutput = String(val)
+        );
+    }
 }
